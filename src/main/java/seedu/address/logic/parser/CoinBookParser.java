1cfb01529492f77d48c34647e299693eddf7e07d
chengyu <laichengyu95@gmail.com>
2018-03-31 00:06:37 +0800

    Replace instances of 'Address' with 'Coin'

--- src/main/java/seedu/address/logic/parser/CoinBookParser.java
+++ src/main/java/seedu/address/logic/parser/CoinBookParser.java
@@ -27,7 +27,7 @@ import seedu.address.logic.parser.exceptions.ParseException;
 /**
  * Parses user input.
  */
-public class AddressBookParser {
+public class CoinBookParser {
 
     /**
      * Used for initial separation of command word and args.
