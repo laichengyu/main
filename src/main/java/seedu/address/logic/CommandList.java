552b55b1b7af6fca8b075ee2c6f9ae79e7c9cd34
chengyu <laichengyu95@gmail.com>
2018-04-13 04:41:34 +0800

    Add basic autocomplete

---
+++ src/main/java/seedu/address/logic/CommandList.java
@@ -0,0 +1,23 @@
+package seedu.address.logic;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Stores a list of all available commands
+ */
+public class CommandList {
+    private List<String> commandList;
+
+    public CommandList() {
+        commandList = Arrays.asList("help", "add", "buy", "sell", "delete", "clear", "tag", "list", "find", "view",
+                "notify", "order", "history", "undo", "redo", "sync", "exit");
+    }
+
+    /**
+     * Returns a defensive copy of {@code commandList}.
+     */
+    public List<String> getList() {
+        return commandList;
+    }
+}
